8bf3770cd9142981d88897c337b84afae9ed5c12
Giulia Campos <38157795+GiuliaCampos@users.noreply.github.com>
2020-08-20 23:31:06 -0300

    Json file e Treemap

--- app/src/App.js
+++ app/src/App.js
@@ -5,20 +5,111 @@ import {
 } from '@material-ui/core'
 import './App.css';
 
-import * as d3 from 'd3';
 import {carregarDados} from './metodosBase'
-
-
+import * as d3 from 'd3';
+import meuDados from './myFile.json' 
 
 function App() {
+  var width, height;
+  width = 800;
+  height = 800;
   const ref = useRef();
-  const [matriculas, setMatriculas] = useState([]);
+  const [sbcData, setSbcData] = useState([]);
 
   useEffect(() => {
     async function loadData(){
-      const aux = await carregarDados();
-      setMatriculas(aux);
-      console.log(matriculas);
+      // const aux = await carregarDados();
+      // setSbcData(aux);
+
+      d3.json(meuDados), function(data){
+
+      
+
+      var partition = data => {
+        const root = d3.hierarchy(data)
+            .sum(d => d.value)
+            .sort((a, b) => b.height - a.height || b.value - a.value);  
+        return d3.partition()
+            .size([height, (root.height + 1) * width / 3])
+          (root);
+      }
+
+      var color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, data.children.length + 1))
+
+      var format = d3.format(",d")
+
+      width = 975
+
+      height = 1200
+
+      const root = partition(data);
+      let focus = root;
+
+      const svg = d3.create("svg")
+          .attr("viewBox", [0, 0, width, height])
+          .style("font", "10px sans-serif");
+
+      const cell = svg
+        .selectAll("g")
+        .data(root.descendants())
+        .join("g")
+          .attr("transform", d => `translate(${d.y0},${d.x0})`);
+
+      const rect = cell.append("rect")
+          .attr("width", d => d.y1 - d.y0 - 1)
+          .attr("height", d => rectHeight(d))
+          .attr("fill-opacity", 0.6)
+          .attr("fill", d => {
+            if (!d.depth) return "#ccc";
+            while (d.depth > 1) d = d.parent;
+            return color(d.data.name);
+          })
+          .style("cursor", "pointer")
+          .on("click", clicked);
+
+      const text = cell.append("text")
+          .style("user-select", "none")
+          .attr("pointer-events", "none")
+          .attr("x", 4)
+          .attr("y", 13)
+          .attr("fill-opacity", d => +labelVisible(d));
+
+      text.append("tspan")
+          .text(d => d.data.name);
+
+      const tspan = text.append("tspan")
+          .attr("fill-opacity", d => labelVisible(d) * 0.7)
+          .text(d => ` ${format(d.value)}`);
+
+      cell.append("title")
+          .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${format(d.value)}`);
+
+      function clicked(p) {
+        focus = focus === p ? p = p.parent : p;
+
+        root.each(d => d.target = {
+          x0: (d.x0 - p.x0) / (p.x1 - p.x0) * height,
+          x1: (d.x1 - p.x0) / (p.x1 - p.x0) * height,
+          y0: d.y0 - p.y0,
+          y1: d.y1 - p.y0
+        });
+
+        const t = cell.transition().duration(750)
+            .attr("transform", d => `translate(${d.target.y0},${d.target.x0})`);
+
+        rect.transition(t).attr("height", d => rectHeight(d.target));
+        text.transition(t).attr("fill-opacity", d => +labelVisible(d.target));
+        tspan.transition(t).attr("fill-opacity", d => labelVisible(d.target) * 0.7);
+      }
+      
+      function rectHeight(d) {
+        return d.x1 - d.x0 - Math.min(1, (d.x1 - d.x0) / 2);
+      }
+
+      function labelVisible(d) {
+        return d.y1 <= width && d.y0 >= 0 && d.x1 - d.x0 > 16;
+      }
+      })
     }
     loadData()
   }, []);
@@ -29,11 +120,7 @@ function App() {
       <Grid container spacing={1} className="App-header">
         <Grid item xs={12}>
           <Typography variant="h3">A participação das mulheres na tecnologia</Typography>
-          <svg viewBox="0 0 100 50" ref={ref}>
-            {matriculas.map(p => (
-              <circle cx={p.fem} cy={p.mas} r="3" key={p.ano} />
-            ))}
-          </svg>
+          <svg  ref={ref} />
         </Grid>
       </Grid> 
     </>
--- app/src/metodosBase.js
+++ app/src/metodosBase.js
@@ -1,10 +1,68 @@
-import data from './csv/concluintesPorGeneroSBC.csv'
+import concluintesGeneroCurso from './csv/concluintesPorGeneroECursoSBC.csv'
+import concluintesGenero from './csv/concluintesPorGeneroSBC.csv'
+import matriculadosGenero from './csv/matriculadosPorGenerpSBC.csv'
+import cursosCriados from './csv/cursosCriadosSBC.csv'
 import * as d3 from 'd3';
 
 export async function carregarDados(){
-    let aux = []
-      await d3.csv(data, function(data){
-        aux.push(data)
-      });
-    return aux;
+    let data = [];
+
+    let aux = [];
+    await d3.csv(concluintesGenero, function(d){
+        aux.push(d)
+    });
+
+    //console.log(aux);
+
+    let aux2 = [];
+    await d3.csv(matriculadosGenero, function(e){
+        aux2.push(e);
+    });
+
+    //console.log(aux2)
+
+    let id = 0;
+
+    aux.forEach(a1 => {
+        aux2.forEach(a2 => {
+            if(a1.ano === a2.ano){
+                data.push({
+                    "name": +a1.ano,
+                    "children": [{
+                        "name": "matriculados",
+                        "value": (+a2.fem)+ (+a1.mas),
+                        "children": [{
+                            "name": "concluintes",
+                            "children": [
+                                {"name": "Mulheres", "value": +a1.fem},
+                                {"name": "Homens", "value": a1.mas},
+                            ]
+                        }]
+                    }],
+                })
+            }
+        })
+    })
+
+    // aux.forEach(a1 => {
+    //     aux2.forEach(a2 => {
+    //         //console.log(a1.Curso + " e " + a2.curso)
+    //         if(a1.Curso === a2.curso){
+    //             if(a1.ano === a2.ano){
+    //                 data.push({
+    //                     id: id,
+    //                     ano: +a1.ano,
+    //                     curso: a2.curso,
+    //                     mulheres: +a1.fem,
+    //                     homens: +a1.mas,
+    //                     criados: +a2.num
+    //                 });
+    //                 id = id+1;
+    //             }
+    //         }
+    //     });
+    // });
+
+    //console.log(data);
+    return data;
 }
